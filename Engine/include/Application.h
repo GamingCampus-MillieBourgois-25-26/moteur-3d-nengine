32341f1321c33710f644d6bc2c4dfd7a4e6d9534
Mathieu Panza <165282376+kuromiaou@users.noreply.github.com>
2026-02-06 11:36:32 +0100

    remove WinAPI

--- Engine/include/Application.h
+++ Engine/include/Application.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <windows.h>
+#include <iostream>
 #include <d3d11.h>
 #include <dxgi.h>
 #include <d3dcompiler.h>
@@ -9,13 +10,6 @@ namespace Engine {
     class Application
     {
     public:
-        ID3D11Device* device = nullptr;
-        ID3D11DeviceContext* context = nullptr;
-        IDXGISwapChain* swapChain = nullptr;
-
-        LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
-        void InitD3D(HWND hwnd);
-        int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPCWSTR, int nCmdShow);
 
         void Run();
     };
--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -2,10 +2,9 @@
 
 int main()
 {
-    // Crée une instance de ton moteur
     Engine::Application app;
 
-    // Lance le moteur (pour l’instant ça ouvre juste la MessageBox)
+
     app.Run();
 
     return 0;
