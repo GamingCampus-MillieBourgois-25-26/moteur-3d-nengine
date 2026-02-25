6b71633c0943b615b26835ef02314cf117acebdd
Quentin LOPES <quentin.lopes@orange.fr>
2026-02-25 18:56:06 +0100

    -add custom math lib in game obj class

--- Engine/include/Engine/GameObject.h
+++ Engine/include/Engine/GameObject.h
@@ -1,18 +1,19 @@
 #pragma once
 #include <iostream>
-#include "Math.h"
+#include "MathsLib/Quaternion.h"
+#include "MathsLib/Vector3.h"
 
 class GameObject {
+private:
+	float x, y, z;
+
 public : 
 
-	struct Transform { Vec3 position; Vec3 rotation; Vec3 scale; };
+	struct Transform { MathsLib::Vector3<float> position; MathsLib::Quaternion<float> rotation; MathsLib::Vector3<float> scale; };
 
 	GameObject();
 	~GameObject();
 
 
-
-
-private : 
 
 };
